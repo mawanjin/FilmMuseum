2f97d24665efc2e5a9b3bcd19962b4571b3c47ea
lala-mac <mwj7999@hotmail.com>
2014-11-02 12:46:42 +0800

    dynamic xml

--- src/com/example/BaseActivity.java
+++ src/com/example/BaseActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import com.example.arthighlights.ArtHighlightsActivity;
+import com.example.data.Info;
 import com.example.data.MagicFactory;
 import com.example.data.Screen;
 import com.example.eagerness.EagernessActivity;
@@ -42,6 +43,8 @@ public class BaseActivity extends Activity implements View.OnClickListener{
      public SlidingMenu menu;
      public ImageView ivMenu;
     private List<Button> buttons = new ArrayList<Button>(0);
+    /** 参观资讯*/
+    private List<Button> button1s = new ArrayList<Button>(0);
 
     public BaseActivity(){}
     public void initSlideMenu(){
@@ -57,6 +60,14 @@ public class BaseActivity extends Activity implements View.OnClickListener{
                 .inflate(R.layout.menu_right, null);
         view.findViewById(R.id.textView1).getWidth();
 
+        //初始化主菜单名称
+        ((Button)view.findViewById(R.id.btn_art)).setText(MagicFactory.getOnlines().get(0).getName());
+        ((Button)view.findViewById(R.id.btn_eag)).setText(MagicFactory.getOnlines().get(1).getName());
+        ((Button)view.findViewById(R.id.btn_navigation)).setText(MagicFactory.getOnlines().get(2).getName());
+        ((Button)view.findViewById(R.id.btn_screening)).setText(MagicFactory.getOnlines().get(3).getName());
+        ((Button)view.findViewById(R.id.btn_information)).setText(MagicFactory.getOnlines().get(4).getName());
+
+        //展映活动
         LinearLayout screeningContainer = (LinearLayout) view.findViewById(R.id.screeningContainer);
         int i=0;
         for(final Screen screen: MagicFactory.getScreens()){
@@ -107,6 +118,55 @@ public class BaseActivity extends Activity implements View.OnClickListener{
             }
             i++;
         }
+        //参观资讯
+        LinearLayout infoContainer = (LinearLayout) view.findViewById(R.id.infoContainer);
+        int k=0;
+        for(final Info info: MagicFactory.getInfos()){
+            Button button = new Button(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0,10,0,0);
+            button.setLayoutParams(params);
+            button.setText(info.getName());
+            button.setTextSize(32);
+            button.setTextColor(getResources().getColor(R.color.gray));
+            button.setBackgroundColor(getResources().getColor(R.color.clear));
+            button.setAlpha(0.3f);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int index = 0;
+                    for(int j=0;j<button1s.size();j++){
+                        if(view == button1s.get(j))index = j;
+                    }
+
+                    Intent intent = new Intent();
+                    intent.putExtra("info", MagicFactory.getInfos().get(index));
+                    if(info.getType()==0){
+                        intent.setClass(BaseActivity.this,
+                                IntroductionActivity.class);
+                    }else{
+                        intent.setClass(BaseActivity.this,
+                                SupServicesActivity.class);
+                    }
+                    startActivity(intent);
+                }
+            });
+            button1s.add(button);
+            infoContainer.addView(button);
+            if(k!=MagicFactory.getScreens().size()-1){
+                ImageView imageView = new ImageView(this);
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.line2));
+                imageView.setLayoutParams(params);
+                screeningContainer.addView(imageView);
+            }else{
+                ImageView imageView = new ImageView(this);
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.line1));
+                imageView.setLayoutParams(params);
+                infoContainer.addView(imageView);
+            }
+            k++;
+        }
 
 
         menu.setMenu(view);
@@ -137,20 +197,20 @@ public class BaseActivity extends Activity implements View.OnClickListener{
 //        view.findViewById(R.id.btn_program).setOnClickListener(this);
         // 参观资讯
         view.findViewById(R.id.btn_information).setOnClickListener(this);
-        // 博物馆简介
-        view.findViewById(R.id.btn_museum).setOnClickListener(this);
-        // 开放时间
-        view.findViewById(R.id.btn_business).setOnClickListener(this);
-        // 购票指南
-        view.findViewById(R.id.btn_guide).setOnClickListener(this);
-        // 配套服务
-        view.findViewById(R.id.btn_supporting).setOnClickListener(this);
-        // 参观须知
-        view.findViewById(R.id.btn_notes).setOnClickListener(this);
-        // 加入我们
-        view.findViewById(R.id.btn_join).setOnClickListener(this);
-        // 联系方式
-        view.findViewById(R.id.btn_phone).setOnClickListener(this);
+//        // 博物馆简介
+//        view.findViewById(R.id.btn_museum).setOnClickListener(this);
+//        // 开放时间
+//        view.findViewById(R.id.btn_business).setOnClickListener(this);
+//        // 购票指南
+//        view.findViewById(R.id.btn_guide).setOnClickListener(this);
+//        // 配套服务
+//        view.findViewById(R.id.btn_supporting).setOnClickListener(this);
+//        // 参观须知
+//        view.findViewById(R.id.btn_notes).setOnClickListener(this);
+//        // 加入我们
+//        view.findViewById(R.id.btn_join).setOnClickListener(this);
+//        // 联系方式
+//        view.findViewById(R.id.btn_phone).setOnClickListener(this);
     }
 
 
@@ -226,48 +286,48 @@ public class BaseActivity extends Activity implements View.OnClickListener{
                 startActivity(intent);
                 finish();
                 break;
-            // 博物馆简介
-            case R.id.btn_museum:
-                intent.setClass(getApplicationContext(), IntroductionActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 开放时间
-            case R.id.btn_business:
-                intent.setClass(getApplicationContext(), BusinessActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 购票指南
-            case R.id.btn_guide:
-                intent.setClass(getApplicationContext(), TicketActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 配套服务
-            case R.id.btn_supporting:
-                intent.setClass(getApplicationContext(), SupServicesActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 参观须知
-            case R.id.btn_notes:
-                intent.setClass(getApplicationContext(), VisitActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 加入我们
-            case R.id.btn_join:
-                intent.setClass(getApplicationContext(), JoinActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            // 联系方式
-            case R.id.btn_phone:
-                intent.setClass(getApplicationContext(), ContactActivity.class);
-                startActivity(intent);
-                finish();
-                break;
+//            // 博物馆简介
+//            case R.id.btn_museum:
+//                intent.setClass(getApplicationContext(), IntroductionActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 开放时间
+//            case R.id.btn_business:
+//                intent.setClass(getApplicationContext(), BusinessActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 购票指南
+//            case R.id.btn_guide:
+//                intent.setClass(getApplicationContext(), TicketActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 配套服务
+//            case R.id.btn_supporting:
+//                intent.setClass(getApplicationContext(), SupServicesActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 参观须知
+//            case R.id.btn_notes:
+//                intent.setClass(getApplicationContext(), VisitActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 加入我们
+//            case R.id.btn_join:
+//                intent.setClass(getApplicationContext(), JoinActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//            // 联系方式
+//            case R.id.btn_phone:
+//                intent.setClass(getApplicationContext(), ContactActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
             default:
                 break;
         }
